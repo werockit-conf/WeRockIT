ec4ab38261c1d22226877a81ecdde9137d78bda9
Raelyn Bangel <raelyn@testdouble.com>
2019-07-18 15:53:38 -0500

    Add black text and teal background

--- WeRockIT-ui/src/App.vue
+++ WeRockIT-ui/src/App.vue
@@ -9,11 +9,19 @@
 @tailwind components;
 @tailwind utilities;
 
+.text-wri-black {
+  color: #191716;
+}
+
 .text-wri-teal {
   color: #5ecec6;
 }
 
 .bg-wri-blue {
   background-color: #05377b;
+}
+
+.bg-wri-teal {
+  background-color: #5ecec6;
 }
 </style>
