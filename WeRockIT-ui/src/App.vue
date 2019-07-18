909043bafab4277ce90b94b73b26edac15dde2f8
Raelyn Bangel <raelyn@testdouble.com>
2019-07-18 10:31:35 -0500

    Pull navbar out

--- WeRockIT-ui/src/App.vue
+++ WeRockIT-ui/src/App.vue
@@ -1,11 +1,5 @@
 <template>
-  <div id="app">
-    <div class="container mx-auto">
-      <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
-      <router-link to="/speakers">Speakers</router-link> | <router-link to="/wip">Schedule</router-link> |
-      <router-link to="/wip">Sponsors</router-link> | <router-link to="/venue">Venue/Travel</router-link> |
-      <router-link to="/wip">Tickets</router-link>
-    </div>
+  <div id="app" class="bg-wri-blue">
     <router-view />
   </div>
 </template>
---
+++ WeRockIT-ui/src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="navbar container mx-auto text-wri-teal">
+    <router-link :to="{ name: 'home' }">Home</router-link> |
+    <router-link :to="{ name: 'about' }">About</router-link> |
+    <router-link :to="{ name: 'speakers' }">Speakers</router-link> |
+    <router-link :to="{ name: 'wip' }">Schedule</router-link> |
+    <router-link :to="{ name: 'wip' }">Sponsors</router-link> |
+    <router-link :to="{ name: 'venue' }">Venue/Travel</router-link> |
+    <router-link :to="{ name: 'tickets' }">Tickets</router-link>
+  </div>
+</template>
+
+<style scoped>
+</style>
