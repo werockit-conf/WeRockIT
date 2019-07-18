de2981b29837c72092f2775e99c898cfabb0eca0
Raelyn Bangel <raelyn@testdouble.com>
2019-07-18 10:38:21 -0500

    Clean up top text

--- WeRockIT-ui/src/views/Home.vue
+++ WeRockIT-ui/src/views/Home.vue
@@ -1,31 +1,25 @@
 <template>
   <div>
     <NavBar></NavBar>
-  <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-    <header class="masthead masthead-background">
-      <div class="container h-100">
-        <div class="row h-100">
-          <div class="col-md-12 my-auto mx-auto">
-            <div class="text-center">
-              <!--<img src="img/WeRockIT_black.png" alt="weRockIT Logo" class="img-responsive img-fluid"/>-->
-              <!--<h1 class="display-1">WeRockIT Conf 2019</h1>-->
-              <h1 class="display-1">WeRockIT Conf 2020</h1>
-              <h2>Developers Conference</h2>
-              <hr />
-              <h5>
-                Cloud Computing | Big Data | Web Development | Cybersecurity |
-                Embedded Systems
-              </h5>
-              <hr />
-              <h3>May 3-4, 2019</h3>
-              <h3>Huntsville, AL</h3>
-              <br />
-              <a
-                href="#mailingList"
-                class="btn btn-outline btn-xl js-scroll-trigger"
-                >Stay in the Loop</a
-              >
+    <div class="home">
+      <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+      <header class="masthead masthead-background">
+        <div class="container h-100 text-wri-teal">
+          <div class="row h-100">
+            <div class="col-md-12 my-auto mx-auto">
+              <div class="text-center">
+                <!--<img src="img/WeRockIT_black.png" alt="weRockIT Logo" class="img-responsive img-fluid"/>-->
+                <!--<h1 class="display-1">WeRockIT Conf 2019</h1>-->
+                <h1 class="uppercase">WeRockIT 2020</h1>
+                <h2 class="uppercase">
+                  DevOps | Development | Data Science | IOT
+                </h2>
+                <hr />
+                <h3 class="uppercase">May 3-4 2019</h3>
+                <h3 class="uppercase">Huntsville, AL</h3>
+                <br />
+                <a href="#mailingList" class="">Stay in the Loop</a>
+              </div>
             </div>
           </div>
         </div>
