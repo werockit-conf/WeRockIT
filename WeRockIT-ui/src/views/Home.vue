3557fe4e473f7d10675c251fb83b10b4c3eaf651
Raelyn Bangel <raelyn@testdouble.com>
2019-07-19 15:54:38 -0500

    Pull views out of home

--- WeRockIT-ui/src/views/Home.vue
+++ WeRockIT-ui/src/views/Home.vue
@@ -1,54 +1,21 @@
 <template>
   <div>
-    <div class="home pt-64 bg-wri-image pb-24">
-      <div class="mx-auto text-wri-teal text-6xl font-bebas font-thin">
-        <div class="text-center">
-          <h1 class="uppercase">WeRockIT 2020</h1>
-          <h2 class="uppercase text-4xl">DevOps | Development | Data Science | IOT</h2>
-          <h3 class="uppercase">May 3-4 2019</h3>
-          <h3 class="uppercase">Huntsville, AL</h3>
-        </div>
-      </div>
-    </div>
-    <div class="container mx-auto bg-wri-blue p-20 text-center">
-      <div class="text-3xl pb-12 text-white">
-        Lorem ipsum dolor sit amet, consectetur adipisc- ing elit. Proin mattis dui convallis molestie plac- erat.
-        Pellentesque tellus ante, volutpat sed plac- erat gravida, ullamcorper eu turpis. Aliquam erat volutpat.
-      </div>
-      <!-- <div class="inline mx-auto p-2 bg-wri-teal rounded-full">Learn more</div> -->
-    </div>
-    <div class="mx-auto bg-white p-20 text-center">
-      <div class="text-6xl font-bebas text-wri-teal">Tracks</div>
-      <div class="flex flex-wrap justify-center text-wri-teal text-5xl uppercase">
-        <div class="flex-shrink-0 bg-wri-dark-blue h-48 w-1/3 m-6 flex justify-center items-center">
-          <div>
-            <div class="font-bebas">DEVOPS</div>
-            <div class="text-base">Cloud, Agile, CI/CD</div>
-          </div>
-        </div>
-        <div class="flex-shrink-0 bg-wri-dark-blue h-48 w-1/3 m-6 flex justify-center items-center">
-          <div>
-            <div class="font-bebas">IOT</div>
-            <div class="text-base">Embedded, Wearables</div>
-          </div>
-        </div>
-        <div class="flex-shrink-0 bg-wri-dark-blue h-48 w-1/3 m-6 flex justify-center items-center">
-          <div>
-            <div class="font-bebas">Development</div>
-            <div class="text-base">Web, Backend, UX</div>
-          </div>
-        </div>
-        <div class="flex-shrink-0 bg-wri-dark-blue h-48 w-1/3 m-6 flex justify-center items-center font-bebas">
-          Data Science
-        </div>
-      </div>
-    </div>
+    <BasicInfo></BasicInfo>
+    <MissionStatement></MissionStatement>
+    <TrackTopics></TrackTopics>
   </div>
 </template>
 
-<style scoped>
-.bg-wri-image {
-  background-image: url('../assets/werockit-web-background.jpg');
-  @apply bg-cover bg-scroll;
+<script>
+import BasicInfo from './Home/BasicInfo.vue'
+import MissionStatement from './Home/MissionStatement.vue'
+import TrackTopics from './Home/TrackTopics.vue'
+
+export default {
+  components: {
+    BasicInfo,
+    MissionStatement,
+    TrackTopics,
+  },
 }
-</style>
+</script>
---
+++ WeRockIT-ui/src/views/Home/TrackTopics.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mx-auto bg-white p-30 text-center">
+    <div class="text-6xl font-bebas text-wri-teal">Tracks</div>
+    <div class="flex flex-wrap justify-center text-wri-teal text-5xl uppercase">
+      <div class="flex-shrink-0 bg-wri-dark-blue h-48 w-1/3 m-6 flex justify-center items-center">
+        <div>
+          <div class="font-bebas">DEVOPS</div>
+          <div class="text-base">Cloud, Agile, CI/CD</div>
+        </div>
+      </div>
+      <div class="flex-shrink-0 bg-wri-dark-blue h-48 w-1/3 m-6 flex justify-center items-center">
+        <div>
+          <div class="font-bebas">IOT</div>
+          <div class="text-base">Embedded, Wearables</div>
+        </div>
+      </div>
+      <div class="flex-shrink-0 bg-wri-dark-blue h-48 w-1/3 m-6 flex justify-center items-center">
+        <div>
+          <div class="font-bebas">Development</div>
+          <div class="text-base">Web, Backend, UX</div>
+        </div>
+      </div>
+      <div class="flex-shrink-0 bg-wri-dark-blue h-48 w-1/3 m-6 flex justify-center items-center font-bebas">
+        Data Science
+      </div>
+    </div>
+  </div>
+</template>
