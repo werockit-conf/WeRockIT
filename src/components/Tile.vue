ebd8935a8f77e5fd449b71cf7a7c0817f6a90390
Raelyn Bangel <raelyn@testdouble.com>
2019-07-30 17:29:23 -0500

    Add tile component

---
+++ src/components/Tile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="bgcolor" class="mx-auto px-24 pt-8 pb-8">
+    <div :class="titleColor" class="text-6xl font-bebas text-center py-2">{{ title }}</div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    bgcolor: {
+      type: String,
+      default: 'bg-white',
+    },
+    titleColor: {
+      type: String,
+      default: 'text-wri-teal',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
