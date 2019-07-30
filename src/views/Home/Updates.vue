56fc1f4906e409ff5d6253938ee92bb710a58f41
Raelyn Bangel <raelyn@testdouble.com>
2019-07-30 17:30:44 -0500

    Add newsletter signup flow

---
+++ src/views/Home/Updates.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-white">
+    <div class="container mx-auto px-32 text-center">
+      <div class="text-6xl font-bebas text-wri-teal">Stay Updated</div>
+      <div class="text-2xl">
+        Sign up for our mailing list. Don't miss the buzz about our speakers, sessions and events! Sign up below to get
+        the latest news about the WeRockIT Conf right in your inbox.
+      </div>
+      <NewsletterSignup></NewsletterSignup>
+    </div>
+  </div>
+</template>
+
+<script>
+import NewsletterSignup from '@/components/NewsletterSignup.vue'
+
+export default {
+  components: { NewsletterSignup },
+}
+</script>
