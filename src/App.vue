bebc8ed583d5406f73ddfc3a95bdf5504c69895c
Raelyn Bangel <raelyn@testdouble.com>
2019-07-30 17:24:47 -0500

    Add Footer

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app" class="bg-wri-blue font-montserrat">
     <NavBar></NavBar>
     <router-view />
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   components: {
     NavBar,
+    Footer,
   },
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="container mx-auto py-6 text-white">Copyright © 2019 WeRockIT Conf</div>
+</template>
