6503e2a517d4318a16a2e9763ceb0cb043c9256b
Angelina Uno-Antonison <ange.unoantonison@gmail.com>
2019-07-29 23:27:37 -0500

    Created intiial draft of a sponsor page with the new design system.

---
+++ src/views/Sponsor/Prospectus.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="mx-auto bg-wri-teal p-20 text-center">
+    <div class="text-3xl pb-12 text-white">
+      Lorem ipsum dolor sit amet, consectetur adipisc- ing elit. Proin mattis dui convallis molestie plac- erat.
+      Pellentesque tellus ante, volutpat sed plac- erat gravida, ullamcorper eu turpis. Aliquam erat volutpat.
+    </div>
+    <a href="./WeRockIT2020SponsorshipProspectus.pdf" target="_blank" class="inline mx-auto p-3 bg-purple-700 rounded-full font-bebas text-white text-3xl">Download Prospectus</a>
+  </div>
+</template>
---
+++ src/views/Sponsor/Sponsors.vue
@@ -0,0 +1,10 @@
+<template>
+<div class="bg-white">
+<div class="w-full">
+      <h2>Sponsors</h2></div>
+    <div class="flex flex-wrap justify-center">
+    <img class="inline-block m-5" src="../../assets/AdtranLogo.png" />
+</div>
+</div>
+
+</template>
