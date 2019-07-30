85554a485d0f28a3d54dc68acf6d4aed528a7f8c
Raelyn Bangel <raelyn@testdouble.com>
2019-07-30 17:23:21 -0500

    Ensure utilities are imported after local css

--- src/App.vue
+++ src/App.vue
@@ -19,31 +19,8 @@ export default {
 <style>
 @tailwind base;
 @tailwind components;
-@tailwind utilities;
 
 @import './assets/css/fonts.css';
 
-.text-wri-black {
-  color: #191716;
-}
-
-.text-wri-dark-blue {
-  color: #1d2152;
-}
-
-.text-wri-teal {
-  color: #5ecec6;
-}
-
-.bg-wri-blue {
-  background-color: #05377b;
-}
-
-.bg-wri-dark-blue {
-  background-color: #1d2152;
-}
-
-.bg-wri-teal {
-  background-color: #5ecec6;
-}
+@tailwind utilities;
 </style>
