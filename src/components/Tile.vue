c8d24a25fe1efb193629d2df432a8357e6042d9b
Raelyn Bangel <raelyn@testdouble.com>
2019-08-01 11:31:28 -0500

    Use compressed titles in mobile

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="bgcolor" class="mx-auto px-8 lg:px-24 pt-8 pb-8">
-    <div :class="titleColor" class="text-6xl font-bebas text-center py-2">{{ title }}</div>
+    <div :class="titleColor" class="text-6xl font-bebas text-center py-2 leading-none">{{ title }}</div>
     <slot></slot>
   </div>
 </template>
