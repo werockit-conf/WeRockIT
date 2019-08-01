fbf9a930d6d282f717119898be49288aa376ee96
Raelyn Bangel <raelyn@testdouble.com>
2019-08-01 08:45:10 -0500

    Move colors into Tailwind config
    Also, I haven't deleted the original colors file on the chance that
    we run into something that doesn't work with this format.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wri-black': '#191716',
+        'wri-blue': '#05377b',
+        'wri-dark-blue': '#1d2152',
+        'wri-dark-purple': '#3b2a60',
+        'wri-teal': '#5ecec6',
+      },
+    },
   },
   variants: {},
   plugins: [],
