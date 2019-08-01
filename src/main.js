ef22b6ef780995ac36eaac62256b56931e94e53f
Angelina Uno-Antonison <ange.unoantonison@gmail.com>
2019-08-01 11:47:23 -0500

    Updated the footer to contain social media links and contact information.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faTwitter, faInstagram, faLinkedin)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
