77551af7796a3884690ca8fe98a1e16236be7b73
Raelyn Bangel <raelyn@testdouble.com>
2019-08-01 11:11:29 -0500

    Clean up
    Still without a blurred image

--- src/views/Home/BasicInfo.vue
+++ src/views/Home/BasicInfo.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="home pt-64 bg-wri-image pb-24">
-    <div class="mx-auto text-wri-teal text-6xl font-bebas font-thin">
+  <div class="home pt-32 lg:pt-64 bg-wri-image pb-24">
+    <div class="mx-auto text-wri-teal font-bebas font-thin">
       <div class="text-center">
-        <h1 class="uppercase">WeRockIT 2020</h1>
-        <h2 class="uppercase text-4xl">DevOps | Development | Data Science | IOT</h2>
-        <h3 class="uppercase">March 26-27 2020</h3>
-        <h3 class="uppercase">Huntsville, AL</h3>
+        <h1 class="uppercase text-6xl lg:text-9xl pt-20 lg:pt-32">WeRockIT 2020</h1>
+        <div class="hidden lg:block">
+          <h2 class="uppercase text-5xl">DevOps | Development | Data Science | IOT</h2>
+        </div>
+        <div class="lg:hidden">
+          <h2 class="uppercase text-3xl">DevOps | Development</h2>
+          <h2 class="uppercase leading-none text-3xl">Data Science | IOT</h2>
+        </div>
+        <h3 class="uppercase text-5xl lg:text-6xl">March 26-27 2020</h3>
+        <h3 class="uppercase leading-none text-5xl lg:text-6xl">Huntsville, AL</h3>
       </div>
     </div>
   </div>
@@ -13,8 +19,18 @@
 
 <style scoped>
 .bg-wri-image {
-  background-image: url('../../assets/werockit-web-background.jpg');
-  @apply bg-cover;
-  @apply bg-scroll;
+  /* background-image: url('../../assets/werockit-web-background.jpg'); */
+  background-image: url('~@/assets/images/landing_background.jpg');
+  background-repeat: no-repeat;
+  background-size: 250%;
+  background-position: 50%;
+}
+
+@screen lg {
+  .bg-wri-image {
+    @apply bg-cover;
+    @apply bg-scroll;
+    background-position: center;
+  }
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
         'wri-dark-purple': '#3b2a60',
         'wri-teal': '#5ecec6',
       },
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '6rem',
+        '9xl': '7rem',
+      },
     },
   },
   variants: {},
