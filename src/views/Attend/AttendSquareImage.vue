ba1e95a978aa997d2437a6a9462199f4e8f3f9e7
serioushorncat <ange.unoantonison@gmail.com>
2019-09-29 11:50:02 -0500

    Refactored the Attend page to use a Square Image block component that is unique to that page that does not involve hover over images darkening of an image since these will not be URL links.

---
+++ src/views/Attend/AttendSquareImage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="box font-roboto-slab" v-bind:style="{ backgroundImage: 'url(' + imagePath() + ')' }">
+    {{ header }}
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  @apply inline-block;
+  @apply w-64;
+  @apply h-64;
+  @apply m-6;
+  @apply p-5;
+  @apply text-center;
+  @apply text-white;
+  @apply bg-black;
+  @apply text-xl;
+  @apply font-semibold;
+}
+</style>
+
+<script>
+export default {
+  name: 'AttendSquareImage',
+  props: {
+    image_path: {
+      type: String,
+      required: true,
+    },
+    header: {
+      type: String,
+      default: ' ',
+    },
+  },
+  methods: {
+    imagePath: function() {
+      return require('@/assets/' + this.image_path)
+    },
+  },
+}
+</script>
--- src/views/Attend/BeyondTheTalks.vue
+++ src/views/Attend/BeyondTheTalks.vue
@@ -1,20 +1,18 @@
 <template>
   <tile bgcolor="bg-white" title="Beyond The Talks" titleColor="text-wri-dark-blue">
-    <div class="flex flex-wrap justify-center">
-      <div class="flex flex-wrap">
-        <SquareImageBlock image_path="images/attend/mothers_room.png" header="Mothers Room" :hover_enabled="false">
+    <div class="justify-center">
+      <div class="">
+        <AttendSquareImage image_path="images/attend/mothers_room.png" header="Mothers Room" :hover_enabled="false">
           <p class="text-base">
             A private mothers room equipped with a refrigerator will be available throughout the event
           </p>
-        </SquareImageBlock>
-        <SquareImageBlock image_path="images/attend/quiet_room.png" header="Quiet Room" :hover_enabled="false">
+        </AttendSquareImage>
+        <AttendSquareImage image_path="images/attend/quiet_room.png" header="Quiet Room" :hover_enabled="false">
           <p class="text-base">
             WeRockIT provides a quiet room to work, read, think, or have much needed recovery time.
           </p>
-        </SquareImageBlock>
-      </div>
-      <div class="flex flex-wrap">
-        <SquareImageBlock
+        </AttendSquareImage>
+        <AttendSquareImage
           image_path="images/attend/speed_mentorship.png"
           header="Speed Mentorship"
           :hover_enabled="false"
@@ -23,30 +21,28 @@
             Pair off with several experienced professionals to gain insight, advice, and knowledge. Preregistration
             required.
           </p>
-        </SquareImageBlock>
-        <SquareImageBlock image_path="images/attend/workshops.png" header="Workshops" :hover_enabled="false">
+        </AttendSquareImage>
+        <AttendSquareImage image_path="images/attend/workshops.png" header="Workshops" :hover_enabled="false">
           <p class="text-base">Learn a new skill. WeRockIT will offer beginner to intermediate level workshops</p>
-        </SquareImageBlock>
-      </div>
-      <div class="flex flex-wrap">
-        <SquareImageBlock image_path="images/attend/social_room.png" header="Social Lounge" :hover_enabled="false">
-        </SquareImageBlock>
-        <SquareImageBlock image_path="images/attend/social_level.jpg" header="Social Level" :hover_enabled="false">
+        </AttendSquareImage>
+        <AttendSquareImage image_path="images/attend/social_room.png" header="Social Lounge" :hover_enabled="false">
+        </AttendSquareImage>
+        <AttendSquareImage image_path="images/attend/social_level.jpg" header="Social Level" :hover_enabled="false">
           <p class="text-base">Establish your level of preferred social interaction with color coded lanyards</p>
-        </SquareImageBlock>
+        </AttendSquareImage>
       </div>
     </div>
   </tile>
 </template>
 
 <script>
-import SquareImageBlock from '@/components/SquareImageBlock.vue'
+import AttendSquareImage from './AttendSquareImage.vue'
 import Tile from '@/components/Tile.vue'
 
 export default {
   name: 'suggested-hotels',
   components: {
-    SquareImageBlock,
+    AttendSquareImage,
     Tile,
   },
 }
