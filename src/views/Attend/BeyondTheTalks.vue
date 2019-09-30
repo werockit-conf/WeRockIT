c1b8678f3ba7b2884b34501ff8d459b9038a16f8
Angelina Uno-Antonison <ange.unoantonison@gmail.com>
2019-09-30 15:20:12 -0500

    Updated the source images to use the cover overlayed ones that we are experimenting with. Attempting to get the sizing to work with larger screens to have them all show either as a single column, a double column, or a column with just three on each row.

--- src/views/Attend/BeyondTheTalks.vue
+++ src/views/Attend/BeyondTheTalks.vue
@@ -1,30 +1,64 @@
 <template>
   <tile bgcolor="bg-white" title="Beyond The Talks" titleColor="text-wri-dark-blue">
-    <div class="justify-center flex flex-wrap ">
-      <AttendSquareImage image_path="images/attend/mothers_room.png" header="Mothers Room" class="attend_block">
+    <div class="justify-center flex flex-wrap xl:hidden">
+      <AttendSquareImage image_path="images/attend/mothers_room.jpg" header="Mothers Room" class="attend_block">
         <p class="text-base">
           A private mothers room equipped with a refrigerator will be available throughout the event
         </p>
       </AttendSquareImage>
-      <AttendSquareImage image_path="images/attend/quiet_room.png" header="Quiet Room" class="attend_block">
+      <AttendSquareImage image_path="images/attend/quiet_room.jpg" header="Quiet Room" class="attend_block">
         <p class="text-base">
           WeRockIT provides a quiet room to work, read, think, or have much needed recovery time.
         </p>
       </AttendSquareImage>
-      <AttendSquareImage image_path="images/attend/speed_mentorship.png" header="Speed Mentorship" class="attend_block">
+      <AttendSquareImage image_path="images/attend/speed_mentorship.jpg" header="Speed Mentorship" class="attend_block">
         <p class="text-base">
           Pair off with several experienced professionals to gain insight, advice, and knowledge. Preregistration
           required.
         </p>
       </AttendSquareImage>
-      <AttendSquareImage image_path="images/attend/workshops.png" header="Workshops" class="attend_block">
+      <AttendSquareImage image_path="images/attend/workshops.jpg" header="Workshops" class="attend_block">
         <p class="text-base">Learn a new skill. WeRockIT will offer beginner to intermediate level workshops</p>
       </AttendSquareImage>
-      <AttendSquareImage image_path="images/attend/social_room.png" header="Social Lounge" class="attend_block">
+      <AttendSquareImage image_path="images/attend/social_room.jpg" header="Social Lounge" class="attend_block">
       </AttendSquareImage>
       <AttendSquareImage image_path="images/attend/social_level.jpg" header="Social Level" class="attend_block">
         <p class="text-base">Establish your level of preferred social interaction with color coded lanyards</p>
       </AttendSquareImage>
+    </div>
+    <div class="">
+      <div class="justify-center xl:flex xl:flex-wrap hidden">
+        <AttendSquareImage image_path="images/attend/mothers_room.jpg" header="Mothers Room" class="attend_block">
+          <p class="text-base">
+            A private mothers room equipped with a refrigerator will be available throughout the event
+          </p>
+        </AttendSquareImage>
+        <AttendSquareImage image_path="images/attend/quiet_room.jpg" header="Quiet Room" class="attend_block">
+          <p class="text-base">
+            WeRockIT provides a quiet room to work, read, think, or have much needed recovery time.
+          </p>
+        </AttendSquareImage>
+        <AttendSquareImage
+          image_path="images/attend/speed_mentorship.jpg"
+          header="Speed Mentorship"
+          class="attend_block"
+        >
+          <p class="text-base">
+            Pair off with several experienced professionals to gain insight, advice, and knowledge. Preregistration
+            required.
+          </p>
+        </AttendSquareImage>
+      </div>
+      <div class="justify-center xl:flex xl:flex-wrap hidden">
+        <AttendSquareImage image_path="images/attend/workshops.jpg" header="Workshops" class="attend_block">
+          <p class="text-base">Learn a new skill. WeRockIT will offer beginner to intermediate level workshops</p>
+        </AttendSquareImage>
+        <AttendSquareImage image_path="images/attend/social_room.jpg" header="Social Lounge" class="attend_block">
+        </AttendSquareImage>
+        <AttendSquareImage image_path="images/attend/social_level.jpg" header="Social Level" class="attend_block">
+          <p class="text-base">Establish your level of preferred social interaction with color coded lanyards</p>
+        </AttendSquareImage>
+      </div>
     </div>
   </tile>
 </template>
