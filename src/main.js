32783053c9ed3b76a6cb607380aabcd1c043b41b
Raelyn Bangel <raelyn@testdouble.com>
2019-10-11 17:31:07 -0500

    Add Google Analytics for basic page views
    This requires adding our tracking ID to a local .env file.
    We still need to ensure we can use this process with Firebase.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
@@ -10,6 +11,17 @@ library.add(faTwitter, faInstagram, faLinkedin)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
+const isProd = process.env.NODE_ENV === 'production'
+const trackingId = process.env.VUE_APP_GOOGLE_ANALYTICS_ID
+
+Vue.use(VueAnalytics, {
+  id: trackingId,
+  router,
+  debug: {
+    enabled: !isProd,
+    sendHitTask: isProd,
+  },
+})
 
 new Vue({
   router,
