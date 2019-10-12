f55045d2c418b31fdcf556b46b3a4e38b6bf6dfb
Raelyn Bangel <raelyn@testdouble.com>
2019-10-11 20:44:55 -0500

    Removed "A Premier Event" (#33)
    https://werockitconf.slack.com/archives/GKA7H7MJ4/p1569973216011900

--- src/views/Home/MissionStatement.vue
+++ src/views/Home/MissionStatement.vue
@@ -3,7 +3,7 @@
     <!-- <div class="container mx-auto bg-wri-blue p-20 text-center"> -->
     <div class="mx-auto text-center text-xl pb-12 text-white">
       <div>
-        A premier event. WeRockIT Conf is an annual two-day conference that features all disciplines of technology and
+        WeRockIT Conf is an annual two-day conference that features all disciplines of technology and
         shows how they all can interconnect. We'll highlight a wide range of speakers to educate and inspire our guests.
         Our mission is to showcase and grow diverse voices in technology.
       </div>
