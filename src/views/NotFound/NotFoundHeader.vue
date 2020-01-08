5235d096fea62abf5ce9a115aaaaa3adfe3b0741
Angelina Uno-Antonison <aunoantonison@uab.edu>
2020-01-08 16:08:53 -0600

    Added a 404 NotFound page when an invalid route is entered within the app.

---
+++ src/views/NotFound/NotFoundHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home pt-64 pb-24">
+    <div class="mx-auto text-white text-6xl font-thin">
+      <div class="text-center">
+        <h1 class="uppercase font-bebas">Page Not Found</h1>
+        <p>404</p>
+      </div>
+    </div>
+  </div>
+</template>
