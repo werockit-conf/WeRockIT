a90c1e133b0867486700096bda1bfc8ce287e680
Angelina Uno-Antonison <aunoantonison@uab.edu>
2020-01-09 16:54:07 -0600

    Designing the keynotes section on the main page which will be used anywhere.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <BasicInfo></BasicInfo>
     <MissionStatement></MissionStatement>
     <TrackTopics></TrackTopics>
+    <Keynotes></Keynotes>
     <Pricing></Pricing>
     <Updates></Updates>
   </div>
@@ -14,10 +15,12 @@ import MissionStatement from './Home/MissionStatement.vue'
 import Pricing from './Home/Pricing.vue'
 import TrackTopics from './Home/TrackTopics.vue'
 import Updates from './Home/Updates.vue'
+import Keynotes from '../components/Keynotes.vue'
 
 export default {
   components: {
     BasicInfo,
+    Keynotes,
     MissionStatement,
     Pricing,
     TrackTopics,
