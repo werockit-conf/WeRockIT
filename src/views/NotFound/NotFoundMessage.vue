12e4f4b4ff2bd55a72c10ebcb819d5b926b62623
Angelina Uno-Antonison <angelina.antonison@gmail.com>
2020-01-12 13:43:25 -0600

    Added a 404 NotFound page when an invalid route is entered within the app. (#41)

---
+++ src/views/NotFound/NotFoundMessage.vue
@@ -0,0 +1,22 @@
+<template>
+  <Tile bgcolor="bg-white" title=":(" titleColor="text-wri-dark-blue">
+    <center>
+      <p>
+        <router-link
+          class="uppercase font-montserrat font-bold block mt-4 inline p-2 bg-wri-teal rounded-full hover:bg-blue-300 text-2xl"
+          :to="{ name: 'home' }"
+        >
+          Return to Home
+        </router-link>
+      </p>
+    </center>
+  </Tile>
+</template>
+
+<script>
+import Tile from '@/components/Tile.vue'
+
+export default {
+  components: { Tile },
+}
+</script>
