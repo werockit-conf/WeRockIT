41fc056eddf00999b2502cd5d6791f107799493c
Angelina Uno-Antonison <aunoantonison@uab.edu>
2020-01-18 07:26:29 -0600

    Worked towards getting the bios section showing content.

--- src/components/Biography.vue
+++ src/components/Biography.vue
@@ -1,18 +1,17 @@
 <template>
-<div class="flex flex-wrap overflow-hidden w-full">
+<div class="flex flex-wrap w-full">
 
-  <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
+  <div class="md:w-1/2">
      <img class="rounded-full shadow-lg mb-5" :src="imagePath()" alt="image" />
   </div>
 
-  <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
-        <div class="font-bold text-lg">{{ name }}</div>
-        <div v-if="job_title != ''" class="text-sm sm:text-md">{{ job_title }}</div>
-        <div v-if="secondary_title != ''" class="text-sm sm:text-md">{{ secondary_title }}</div>
-        <div v-if="company != ''" class="italic text-md sm:text-lg">{{ company }}</div>
-        <br />
-        <div> {{ talk_title }} </div>
-        <div v-if="biography != ''" class="text-sm">{{ biography }}</div>
+  <div class="overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
+    <div class="font-bold text-lg">{{ name }}</div>
+    <div v-if="job_title != ''" class="text-sm sm:text-md">{{ job_title }}</div>
+    <div v-if="secondary_title != ''" class="text-sm sm:text-md">{{ secondary_title }}</div>
+    <div v-if="company != ''" class="italic text-md sm:text-lg">{{ company }}</div>
+    <div class="mt-3"> {{ talk_title }} </div>
+    <div v-if="biography != ''" class="text-sm">{{ biography }}</div>
   </div>
 
 </div>
