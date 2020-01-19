8c882deafc17f2beac2b2532bc26ada95973b7b8
Shinichi Uno <uno.shinichi11@gmail.com>
2020-01-18 20:14:08 -0600

    Updated formatting of biography for both mobile and desktop.

--- src/components/Biography.vue
+++ src/components/Biography.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="flex flex-wrap w-full">
-    <div class="md:w-1/2">
+  <div class="flex flex-wrap w-full mt-5">
+    <div class="md:w-1/4 mr-5">
       <img class="rounded-full shadow-lg mb-5" :src="imagePath()" alt="image" />
     </div>
-
-    <div class="overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
-      <div class="font-bold text-lg">{{ speakerName }}</div>
-      <div v-if="job_title != ''" class="text-sm sm:text-md">{{ job_title }}</div>
-      <div v-if="secondary_title != ''" class="text-sm sm:text-md">{{ secondary_title }}</div>
-      <div v-if="company != ''" class="italic text-md sm:text-lg">{{ company }}</div>
-      <div class="mt-3">{{ talk_title }}</div>
-      <div v-if="biography != ''" class="text-sm">{{ biography }}</div>
+    <div class="overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">    
+      <span class="text-center md:text-left">
+        <div class="text-2xl font-bold tracking-tighter md:mt-5 tracking-normal">{{ speakerName }}</div>
+        <div v-if="job_title != ''" class="text-lg font-medium">{{ job_title }}</div>
+        <div v-if="secondary_title != ''" class="text-lg font-medium">{{ secondary_title }}</div>
+        <div v-if="company != ''" class="text-lg font-medium">{{ company }}</div>
+        <div class="mt-3 mb-2 text-lg font-bold">{{ talk_title }}</div>
+      </span>
+      <div v-if="biography != ''" class="text-sm tracking-wide">{{ biography }}</div>
     </div>
   </div>
 </template>
