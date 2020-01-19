99a5262213565d4e3e691df5ea5e6b33213d6581
Angelina Uno-Antonison <angelina.antonison@gmail.com>
2020-01-18 19:36:18 -0600

    Close mobile drop menu after click (#47)
    * The menu for mobile now will toggle close if a route is being exited.  It wont open if it is already closed.
    
    * Removed console long.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,11 +2,13 @@
   <div :class="displayClasses" class="z-50">
     <nav ref="logo" class="flex items-center justify-between flex-wrap">
       <div class="mx-auto inline float-left">
-        <img
-          alt="WeRockIT Conference"
-          src="../assets/WeRockIT_logo.png"
-          class="h-20 sm:float-left lg:h-24 lg:mx-12 lg:my-8"
-        />
+        <NavBarLogo name="home">
+          <img
+            alt="WeRockIT Conference"
+            src="../assets/WeRockIT_logo.png"
+            class="h-20 sm:float-left lg:h-24 lg:mx-12 lg:my-8"
+          />
+        </NavBarLogo>
       </div>
       <div class="block lg:hidden mx-auto my-8 inline">
         <button
@@ -56,9 +58,10 @@
 
 <script>
 import NavBarLink from './NavBarLink.vue'
+import NavBarLogo from './NavBarLogo.vue'
 
 export default {
-  components: { NavBarLink },
+  components: { NavBarLink, NavBarLogo },
   data: function() {
     return {
       open: false,
@@ -85,6 +88,13 @@ export default {
       let scrolled = window.scrollY > this.navbarHeight
       this.bgcolor = scrolled ? 'bg-white' : 'bg-transparent'
       this.textColor = scrolled ? 'text-black' : 'text-white'
+    },
+  },
+  watch: {
+    $route() {
+      if (this.open) {
+        this.toggle()
+      }
     },
   },
   mounted() {
