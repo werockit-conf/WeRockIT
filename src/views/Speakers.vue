43ccb78fe4a57ffef5b25631e23bb8c664f6eae2
Angelina Uno-Antonison <aunoantonison@uab.edu>
2020-01-19 09:20:53 -0600

    wip for adding the floaty button

--- src/views/Speakers.vue
+++ src/views/Speakers.vue
@@ -3,8 +3,29 @@
     <SpeakersHeader></SpeakersHeader>
     <TheSpeakers :speakers="this.speakers"></TheSpeakers>
     <Biographies :speakers="this.speakers"></Biographies>
+    <button class="floatybutton">fdsfsd</button>
   </div>
 </template>
+
+
+<style scoped>
+.floatybutton {
+  @apply fixed;
+  /* @apply hidden; */
+  @apply text-center;
+  @apply bottom-0;
+  @apply right-0;
+  @apply z-50;
+  @apply text-base;
+  @apply border-0;
+  @apply outline-none;
+  @apply bg-red-600;
+  @apply text-white;
+  @apply cursor-pointer;
+  @apply rounded-sm;
+  @apply p-10;
+}
+</style>
 
 <script>
 import SpeakersHeader from './Speakers/SpeakersHeader.vue'
