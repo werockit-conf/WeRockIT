7346d74dc895d9d0432f5b02c6935fef1034804d
x86octopus <48194186+x86octopus@users.noreply.github.com>
2020-01-24 12:34:58 -0600

    centered keynotes in lg and added more speakers button

--- src/components/Keynotes.vue
+++ src/components/Keynotes.vue
@@ -1,16 +1,26 @@
 <template>
   <Tile bgcolor="bg-white" title="Keynotes" titleColor="text-wri-teal">
-    <div class="sm:flex md:flex-wrap justify-center">
-      <SpeakerCard
-        v-for="person in keynotes"
-        :key="person.id"
-        class="sm:w-1/3"
-        :image_path="person.image_path"
-        :name="person.name"
-        :talk_title="person.talkTitle"
-        :anchor_name="person.anchorName"
-        :allow_route_history="true"
-      />
+    <div class="container mx-auto pt-5">
+      <div class="sm:flex md:flex-wrap justify-center">
+        <SpeakerCard
+          v-for="person in keynotes"
+          :key="person.id"
+          class="sm:w-1/3"
+          :image_path="person.image_path"
+          :name="person.name"
+          :talk_title="person.talkTitle"
+          :anchor_name="person.anchorName"
+          :allow_route_history="true"
+        />
+      </div>
+    </div>
+    <div class="container mx-auto text-center lg:p-20 md:p-16 sm:p-12">
+      <a
+        href="/speakers"
+        target="_blank"
+        class="uppercase font-bold mt-4 font-montserrat inline p-2 px-6 bg-wri-teal rounded-full hover:bg-blue-300  text-2xl"
+        >More Speakers ></a
+      >
     </div>
   </Tile>
 </template>
