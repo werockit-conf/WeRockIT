655735fc2f4e673a13c680268061e9eea56b32f2
Raelyn Bangel <raelyn@testdouble.com>
2020-03-10 18:42:15 -0500

    add info page

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <div class="font-medium text-lg inline">
         As event organizers, we have come to the decision to reschedule WeRockIT Conf 2020 due to concerns about the
         spread of COVID-19 and its impact on domestic and international travel.
-        <a href="foo" class="text-blue-600">Learn more</a>
+        <NavBarLink class="text-blue-600 normal-case" name="Covid19Update">Learn more</NavBarLink>
       </div>
     </Banner>
     <router-view />
@@ -18,12 +18,14 @@
 // @ is an alias to /src
 import Banner from '@/components/Banner.vue'
 import NavBar from '@/components/NavBar.vue'
+import NavBarLink from '@/components/NavBarLink.vue'
 import Footer from '@/components/Footer.vue'
 import GenerateLetterFooter from '@/components/GenerateLetterFooter.vue'
 
 export default {
   components: {
     NavBar,
+    NavBarLink,
     Banner,
     Footer,
     GenerateLetterFooter,
--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute left-0 right-0">
+  <div v-if="shouldDisplay" class="absolute left-0 right-0">
     <div class="relative bg-indigo-200 mt-32 mx-16 rounded-lg">
       <div class="p-4">
         <slot></slot>
@@ -7,3 +7,13 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    shouldDisplay() {
+      return this.$route.name != 'Covid19Update'
+    },
+  },
+}
+</script>
